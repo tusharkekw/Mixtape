cbd48eaf7072f4f4cce8fda7c03f49e7754ebf2b
tusharkekw <tushar.khatri.it19@nsut.ac.in>
2025-12-25 21:04:22 +0530

    stop tracking dist directory

--- server/dist/lib/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const adapter_pg_1 = require("@prisma/adapter-pg");
-const client_1 = require("@prisma/client");
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new adapter_pg_1.PrismaPg({ connectionString });
-const prisma = new client_1.PrismaClient({ adapter });
-exports.default = prisma;
